bf5ffc9babbd5fe8c5840d3e850e0b6a2c26deb3
Yogender21505 <yogender21505@iiitd.ac.in>
2022-11-22 17:47:05 +0530

    new file

--- Assignment02/1.2/Fork_Exec.c
+++ Assignment02/1.2/Fork_Exec.c
@@ -9,7 +9,7 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-
+#define BILLION  1000000000L;
 void removeWhiteSpace(char* buf){
 	if(buf[strlen(buf)-1]==' ' || buf[strlen(buf)-1]=='\n')
 	buf[strlen(buf)-1]='\0';
@@ -43,12 +43,21 @@ void executeBasic(char** argv){
 		exit(1);
 	}
 }
+void print(double *hist){
+    for (int i=0;i<sizeof(hist)/sizeof(hist[0]);i++){
+        printf("runtime taken by processes %lf",hist[i]);
+    }
 
+}
 
 int main(char** argv,int argc)
 {
 	char buf[500],*buffer[100],buf2[500],buf3[500], *params1[100],*params2[100],*token,cwd[1024];
 	int nr=0;
+    double hist[3];
+    int pos=0;
+    struct timespec start, stop;
+    double accum;
     FILE* filePointer; 
     filePointer = fopen("compilationofKernel.sh", "r");
     pid_t pid, pid1, pid2;
@@ -69,7 +78,21 @@ int main(char** argv,int argc)
 
                 }
                 else{
-                    waitpid(pid,NULL,WUNTRACED);
+                    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+                    perror( "clock gettime" );
+                    return ;
+                    }
+                    waitpid(pid,NULL,WUNTRACED)
+                    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+                    perror( "clock gettime" );
+                    return ;
+                    }
+                        accum = ( stop.tv_sec - start.tv_sec )
+                        + (double)( stop.tv_nsec - start.tv_nsec )
+                        / (double)BILLION;
+                // printf( "%lf\n", accum );
+                        hist[pos++]=accum;
+                    
                     kill(pid,SIGSTOP);
                 }
 
@@ -88,8 +111,22 @@ int main(char** argv,int argc)
 
             }
             else{
+                if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+                perror( "clock gettime" );
+                return ;
+                }
                 waitpid(pid1,NULL,WUNTRACED);
+                if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+                perror( "clock gettime" );
+                return ;
+                }
+                    accum = ( stop.tv_sec - start.tv_sec )
+                        + (double)( stop.tv_nsec - start.tv_nsec )
+                        / (double)BILLION;
+                // printf( "%lf\n", accum );
+                hist[pos++]=accum;
                 kill(pid1,SIGSTOP);
+                
             }
     }
 
@@ -106,11 +143,25 @@ int main(char** argv,int argc)
 
             }
             else{
+                if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+                perror( "clock gettime" );
+                return ;
+                }
                 waitpid(pid2,NULL,WUNTRACED);
+                if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+                perror( "clock gettime" );
+                return ;
+                }
+                    accum = ( stop.tv_sec - start.tv_sec )
+                        + (double)( stop.tv_nsec - start.tv_nsec )
+                        / (double)BILLION;
+                // printf( "%lf\n", accum );
+                hist[pos++]=accum;
                 kill(pid2,SIGSTOP);
             }
     }
     else{
+        print(hist);
         exit(0);
     }
 
