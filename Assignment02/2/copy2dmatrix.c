18878cfb4e1e57d7a4c333468969aa9d74693086
Yogender21505 <yogender21505@iiitd.ac.in>
2022-11-24 22:55:53 +0530

    final updated code

---
+++ Assignment02/2/copy2dmatrix.c
@@ -0,0 +1,30 @@
+#include <linux/syscalls.h>
+#include <stdio.h>
+
+
+#define kernel_2d_memcpy =451;
+// void check(float one[3][3],float two[3][3]){
+//   for (int i=0;i<3;i++){
+//     for(int j=0;j<3;j++){
+//       printf("%lf  %lf ",one[i][j],two[i][j]);
+//     }
+//     printf("\n");
+//   }
+// }
+
+int main ()    
+{    
+    float src[][3]={ {1,2,3},
+                      {4,5,6},
+                      {8,9,10}};
+
+    float dest[][3]={{11,12,13},
+                      {14,15,16},
+                      {17,18,19}};
+    int i,j;
+    long sys=syscalls(kernel_2d_memcpy,dest,src,3,3);
+    check(src,dest);
+    if (!sys){
+      printf("451 system call successful\n");
+    }
+}
