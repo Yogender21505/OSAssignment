a245949e7402dbf3425d7d99502e9e5200464364
Yogender21505 <yogender21505@iiitd.ac.in>
2022-11-24 23:07:08 +0530

    FINAL CODE

--- Assignment02/2/copy2dmatrix.c
+++ Assignment02/2/copy2dmatrix.c
@@ -3,14 +3,14 @@
 
 
 #define kernel_2d_memcpy =451;
-// void check(float one[3][3],float two[3][3]){
-//   for (int i=0;i<3;i++){
-//     for(int j=0;j<3;j++){
-//       printf("%lf  %lf ",one[i][j],two[i][j]);
-//     }
-//     printf("\n");
-//   }
-// }
+void check(float two[3][3]){
+  for (int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      printf("%lf ",two[i][j]);
+    }
+    printf("\n");
+  }
+}
 
 int main ()    
 {    
@@ -22,9 +22,15 @@ int main ()
                       {14,15,16},
                       {17,18,19}};
     int i,j;
+    printf("copying 2d matrix\n");
     long sys=syscalls(kernel_2d_memcpy,dest,src,3,3);
-    check(src,dest);
+    check(dest);
     if (!sys){
       printf("451 system call successful\n");
     }
 }
+
+
+/*Reference 
+BOOKS,LINKS OF TUTORIALS,ONLINE PROBLEMS SOLVING WEBSITE SUCH AS STACKOVERFLOW
+*/
