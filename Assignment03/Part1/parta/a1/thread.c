787071f9e424876f60bb63865c24b3a0ec367e4c
Yogender21505 <yogender21505@iiitd.ac.in>
2022-12-19 14:27:45 +0530

    updated 1a1

--- Assignment03/Part1/parta/a1/thread.c
+++ Assignment03/Part1/parta/a1/thread.c
@@ -14,8 +14,8 @@ int main(){
     for(i=1;i<=5;i++){
         pthread_mutex_init(&Forks[i],NULL); // Initialized the mutex for thread
         pthread_create(&philosopher[i],NULL,func,(void*)&i); // Create pthread for Philospher
-        sleep(2);
         pthread_join(philosopher[i],&msg); // Joining pthread one by one 
+        sleep(3);
     }
 
     return 0;
@@ -24,12 +24,10 @@ void * func(void * i){
     // printf("\nPhilosopher %d is thinking ",n);
     //Take Fork
     int n=*(int *)i;
+    printf("\nPhilosopher %d is Eating ",n);
     pthread_mutex_lock(&Forks[n]);          // when philosopher 5 is eating he takes fork 1 and fork 5
     pthread_mutex_lock(&Forks[(n+1)%5]);
     //Eating
-    printf("\nPhilosopher %d is eating ",n);
-
-    sleep(3);
     //Putting Fork
     pthread_mutex_unlock(&Forks[n]);
     pthread_mutex_unlock(&Forks[(n+1)%5]);
