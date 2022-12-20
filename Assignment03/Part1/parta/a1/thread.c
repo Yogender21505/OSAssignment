076fdae6f56f07c053424d88f211608f9d7a940a
Yogender21505 <yogender21505@iiitd.ac.in>
2022-12-20 20:13:44 +0530

    commit all final

--- Assignment03/Part1/parta/a1/thread.c
+++ Assignment03/Part1/parta/a1/thread.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-#include<semaphore.h>
 #include<unistd.h>
 
 pthread_t philosopher[5];
-pthread_mutex_t Forks[5];
+int Forks[5]={0};
 void * func(void * i);
 
 int main(){
@@ -13,10 +12,9 @@ int main(){
     void *msg;
     //@https://stackoverflow.com/questions/29050328/pthread-join-function-in-c
     for(i=1;i<=5;i++){
-        pthread_mutex_init(&Forks[i],NULL); // Initialized the mutex for thread
         pthread_create(&philosopher[i],NULL,func,(void*)&i); // Create pthread for Philospher
         pthread_join(philosopher[i],&msg); // Joining pthread one by one 
-        sleep(3);
+        sleep(2);
     }
 
     return 0;
@@ -25,16 +23,17 @@ void * func(void * i){
     // printf("\nPhilosopher %d is thinking ",n);
     //Take Fork
     int n=*(int *)i;
-    printf("\nPhilosopher %d is Eating ",n);
-    pthread_mutex_lock(&Forks[n]);          // when philosopher 5 is eating he takes fork 1 and fork 5
-    pthread_mutex_lock(&Forks[(n+1)%5]);
-    //Eating
-    //Putting Fork
-     //@ https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
-    pthread_mutex_unlock(&Forks[n]);
-    pthread_mutex_unlock(&Forks[(n+1)%5]);
-
-    //Finished Eating
+    
+    if (Forks[n]==0 && Forks[(n+1)%5]==0){
+        printf("\nPhilosopher %d is Eating ",n);
+        Forks[n]=1;
+        Forks[(n+1)%5]=1;
     printf("\nPhilosopher %d Finished eating ",n);
+    Forks[n]=0;
+        Forks[(n+1)%5]=0;
 
+    }else{
+        sleep(3);
+    }       
+     //@ https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
 }
--- Assignment03/Part1/partb/b1/thread.c
+++ Assignment03/Part1/partb/b1/thread.c
@@ -1,44 +1,83 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-#include<semaphore.h>
 #include<unistd.h>
 
 pthread_t philosopher[5];
-pthread_mutex_t Forks[5];
+int philosopher_flag[5]={0};
+int Forks[5]={0};
 void * func(void * i);
-
+int bowl[2]={0};
+int i;
 int main(){
-    int i,k;
+    int k;
     void *msg;
+    
     //@https://stackoverflow.com/questions/29050328/pthread-join-function-in-c
-    for(i=1;i<=5;i++){
-        pthread_mutex_init(&Forks[i],NULL); // Initialized the mutex for thread
+    for(i=1;i<=4;i++){
         pthread_create(&philosopher[i],NULL,func,(void*)&i); // Create pthread for Philospher
-        pthread_join(philosopher[i],&msg); // Joining pthread one by one 
-        // sleep(3);
+        sleep(3);
+        pthread_join(philosopher[i],NULL); // Joining pthread one by one 
+        sleep(2);
     }
 
+    if(i==5){
+        printf("\nPhilosopher %d is Eating ",5);
+        printf("\nPhilosopher %d Finished eating ",5);
+    }
     return 0;
 }
 void * func(void * i){
     // printf("\nPhilosopher %d is thinking ",n);
     //Take Fork
     int n=*(int *)i;
-    printf("\nPhilosopher %d is Eating ",n);
-    pthread_mutex_lock(&Forks[n]);          // when philosopher 5 is eating he takes fork 1 and fork 5
-    pthread_mutex_lock(&Forks[(n+1)%5]);
-    //Eating
-    //Putting Fork
-     //@ https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
-    pthread_mutex_unlock(&Forks[n]);
-    pthread_mutex_unlock(&Forks[(n+1)%5]);
-    sleep(1);
-    pthread_mutex_lock(&Forks[n]);          // when philosopher 5 is eating he takes fork 1 and fork 5
-    pthread_mutex_lock(&Forks[(n+1)%5]);
-    //Finished Eating
-    pthread_mutex_unlock(&Forks[n]);
-    pthread_mutex_unlock(&Forks[(n+1)%5]);
-    printf("\nPhilosopher %d Finished eating ",n);
-}
+    if (bowl[0]==0){
+        n=*(int *)i;
+            if (Forks[n]==0 && Forks[(n+1)%5]==0){
+                printf("\nPhilosopher %d is Eating ",n);
 
+                Forks[n]=1;
+                Forks[(n+1)%5]=1;
+                bowl[0]=1;
+                printf("\nPhilosopher %d Finished eating ",n);
+                Forks[n]=0;
+                Forks[(n+1)%5]=0;
+                bowl[0]=0;
+
+            }
+            else{
+                sleep(3);
+            } 
+            if (*(int *)i==5){
+                exit(1);
+            }
+    }
+    if (bowl[1]==0){
+            *(int *)i=*(int *)i+1;
+           n=*(int *)i;
+            if (Forks[n]==0 && Forks[(n+1)%5]==0){
+                printf("\nPhilosopher %d is Eating ",n);
+                if (n==5){
+                    printf("\nPhilosopher %d Finished eating ",n);
+                    exit(1);
+                }
+                Forks[n]=1;
+                Forks[(n+1)%5]=1;
+                bowl[1]=1;
+                printf("\nPhilosopher %d Finished eating ",n);
+                Forks[n]=0;
+                Forks[(n+1)%5]=0;
+                bowl[1]=0;
+
+            }
+            else{
+                sleep(3);
+            } 
+            if (*(int *)i==5){
+                exit(1);
+            }
+    }
+
+      
+     //@ https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
+}
